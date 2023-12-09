a21e49c8c2f7ea6ef84f297d2512bffcaa70bf0f
Utente <Utente@DESKTOP-1NF2F0Q.homenet.telecomitalia.it>
2023-12-09 11:58:28 +0100

    fine compiti per casa classe e main BigliettoFerroviario

--- src/biglietto/BigliettoFerroviario.java
+++ src/biglietto/BigliettoFerroviario.java
@@ -1,45 +1,88 @@
 package biglietto;
 
 /**
- *
+ * Classe che calcola il costo di un biglietto ferroviario in base a se si è uno studente, pensionato o disoccupato
  * @author luca.negriolli
+ * @version 1.0
  */
+
 public class BigliettoFerroviario {
     private float tariffa;
     private String ruolo;
+    
+    /**
+     * Costruttore senza parametri
+     */
 
     public BigliettoFerroviario() {
         
     }
+    
+    /**
+     * Costruttore con parametri
+     * @param tariffa
+     * @param ruolo 
+     */
 
     public BigliettoFerroviario(float tariffa, String ruolo) {
         this.tariffa = tariffa;
         this.ruolo = ruolo;
     }
+    
+    /**
+     * Metodo che visualizza il contenuto della variabile tariffa
+     * @return 
+     */
 
     public float getTariffa() {
         return tariffa;
     }
+    
+    /**
+     * Metodo che imposta/modifica il contenuto della variabile tariffa
+     * @param tariffa 
+     */
 
     public void setTariffa(float tariffa) {
         this.tariffa = tariffa;
     }
+    
+    /**
+     * Metodo che visualizza il contenuto della variabile ruolo
+     * @return 
+     */
 
     public String getRuolo() {
         return ruolo;
     }
+    
+    /**
+     * Metodo che imposta/modifica il contenuto della variabile ruolo
+     * @param ruolo 
+     */
 
     public void setRuolo(String ruolo) {
         this.ruolo = ruolo;
     }
     
+    /**
+     * MetoMetodo che visualizza il contenuto dei dati inseriti in input
+     * @return 
+     */
+    
     public String info(){
         String testo;
         
-        testo = "La tariffa è pari a: " + tariffa + "\n";
+        testo = "La tariffa è pari a: " + tariffa + "\n" +
+                "Il ruolo è: " + ruolo + "\n";
         
         return testo;
     }
+    
+    /**
+     * Metodo che serve a calcolare l'importo del biglietto ferroviario scontato
+     * @return 
+     */
      
     public float costoBiglietto(){
         float costo;
@@ -48,25 +91,17 @@ public class BigliettoFerroviario {
         if(ruolo.equals("Pensionato"))
             sconto = (tariffa/100)*10;
             costo  = tariffa-sconto;
+            
         if(ruolo.equals("Studente"))
             sconto = (tariffa/100)*15;
             costo  = tariffa-sconto;
+            
         if(ruolo.equals("Disoccupato"))
             sconto = (tariffa/100)*25;
             costo  = tariffa-sconto;
             
         return costo;           
-    }
-    
-  
-    
-    
-    
-    
-    
-    
-
-    
+    } 
     
 }
 
--- src/biglietto/BigliettoFerroviarioTest.java
+++ src/biglietto/BigliettoFerroviarioTest.java
@@ -11,8 +11,20 @@ public class BigliettoFerroviarioTest {
         float tariffa;
         String ruolo;
         
-        System.out.println("Inserire la tariffa");
+        System.out.print("Inserire la tariffa: ");
         tariffa = in.nextFloat();
+        
+        System.out.print("Inserire il ruolo: ");
+        ruolo = in.nextLine();
+        ruolo = in.nextLine();
+        
+        BigliettoFerroviario bf = new BigliettoFerroviario(tariffa,ruolo);
+        
+        System.out.println("Dati in input: ");
+        System.out.println(bf.info());
+        
+        System.out.println("Dati di output: ");
+        System.out.println("Ecco il costo del biglietto scontato: " + bf.costoBiglietto() + "€");
         
     }
     
