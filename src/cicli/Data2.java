07e304d56d5367cb7fc5cf0ee58cb809b4d309a9
luca.negriolli <luca.negriolli@AULA310-PC12.itib.local>
2024-03-08 15:39:15 +0100

    Fine Sportello Informatica 08/03/2024 classe data2 e data2Test

--- src/cicli/Data2.java
+++ src/cicli/Data2.java
@@ -1,52 +1,28 @@
 package cicli;
 
 /**
- * Classe che scrive una data controllando se è valida
  *
- * @author luca.negriolli 3INA 2024
- * @version 1.0
+ * @author david.ober
  */
 public class Data2 {
 
-    private int a;
-    private int m;
     private int g;
+    private int m;
+    private int a;
     private int giorno;
     private int mese;
     private int anno;
-    private int giorno1;
-    private int mese1;
-    private int anno1;
-    
+
     public Data2() {
     }
 
-    public Data2(int a, int m, int g, int giorno, int mese, int anno, int giorno1, int mese1, int anno1) {
-        this.a = a;
-        this.m = m;
+    public Data2(int g, int m, int a, int giorno, int mese, int anno) {
         this.g = g;
+        this.m = m;
+        this.a = a;
         this.giorno = giorno;
         this.mese = mese;
         this.anno = anno;
-        this.giorno1 = giorno1;
-        this.mese1 = mese1;
-        this.anno1 = anno1;
-    }
-
-    public int getA() {
-        return a;
-    }
-
-    public void setA(int a) {
-        this.a = a;
-    }
-
-    public int getM() {
-        return m;
-    }
-
-    public void setM(int m) {
-        this.m = m;
     }
 
     public int getG() {
@@ -57,95 +33,119 @@ public class Data2 {
         this.g = g;
     }
 
-    public static boolean isValida() {
-        boolean is = false;
+    public int getM() {
+        return m;
+    }
 
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getGiorno() {
+        return giorno;
+    }
+
+    public void setGiorno(int giorno) {
+        this.giorno = giorno;
+    }
+
+    public int getMese() {
+        return mese;
+    }
+
+    public void setMese(int mese) {
+        this.mese = mese;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public boolean isValido() {
+        boolean valido = false;
         if (a < 1000 || a > 9999) {
-            is = false;
-        }
+            valido = false;
 
+        }
         if (m < 1 || m > 12) {
-            is = false;
+            valido = false;
         }
 
         if (m == 4 || m == 6 || m == 9 || m == 11) {
-            if (g >= 1 && g <= 30) {
-                is = true;
+            if (g >= 1 || g <= 30) {
+                valido = true;
             }
         }
-
         if (m == 1 || m == 2 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-            if (g >= 1 && g <= 31) {
-                is = true;
+            if (g >= 1 || g <= 31) {
+                valido = true;
             }
         }
 
-        if (a % 4 == 0) {
-            if (m == 2) {
-                if (g >= 1 && g <= 28) {
-                    is = true;
-                }
-            }
-        }
-
-        return is;
+        return valido;
     }
 
-    public static boolean isBisestile() {
-        boolean is = false;
-        
+    public boolean isValido2() {
+        boolean valido = false;
+        if (anno < 1000 || anno > 9999) {
+            valido = false;
+
+        }
+        if (mese < 1 || mese > 12) {
+            valido = false;
+        }
+
+        if (mese == 4 || mese == 6 || mese == 9 || mese == 11) {
+            if (giorno >= 1 || giorno <= 30) {
+                valido = true;
+            }
+        }
+        if (mese == 1 || mese == 2 || mese == 3 || mese == 5 || mese == 7 || mese == 8 || mese == 10 || mese == 12) {
+            if (giorno >= 1 || giorno <= 31) {
+                valido = true;
+            }
+        }
+
+        return valido;
+    }
+
+    public boolean isBisestile() {
+        boolean is = true;
         if (a % 400 == 0 || (a % 4 == 0 && !(a % 100 == 0))) {
             is = true;
-        }else{
+        } else {
             is = false;
         }
-    
         return is;
     }
-    
-    public int nGiorni(){
-        int nGiorni = 0;       
-        int [] data1 = {giorno, mese, anno};
-        int [] data2 = {giorno1, mese1, anno1};
-        
-        if(anno > anno1){
-            int [] data1 = {giorno, mese, anno};
-            int [] data2 = {giorno1, mese1, anno1};    
-        }else{
-            if(mese > mese1){
-                
-            }else{
-                if
-            }
-            int [] data1 = {giorno, mese, anno};
-            int [] data2 = {giorno1, mese1, anno1};
+
+    public boolean isBisestile2() {
+        boolean is = true;
+        if (anno % 400 == 0 || (anno % 4 == 0 && !(anno % 100 == 0))) {
+            is = true;
+        } else {
+            is = false;
         }
-        
-        while(true){
-            if(data1 == data2){
-                break;
-            }
-            nGiorni++;
-            giorno += 1;
-            if(isValida() == false){
-                giorno = 0;
-                mese += 1;
-                if(mese == 13){
-                    mese = 0;
-                    anno++;
-                }
-            }
-        }
-        
-        return nGiorni;
+        return is;
     }
 
     public String info() {
         String testo = "";
-
-        if (isValida() == true) {
+        if (isValido() == true) {
             if (g >= 1 && g <= 9) {
-                testo += "0" + g + "/";
+                testo = testo = "0" + g + "/";
             } else {
                 testo = g + "/" + m + "/" + a;
             }
@@ -153,11 +153,71 @@ public class Data2 {
                 testo += "0" + m + "/" + a;
             } else {
                 testo = g + "/" + m + "/" + a;
-            } 
+            }
         } else {
-            testo = "Data Invalida";
+            testo = "data non valida";
         }
         return testo;
     }
 
+    public String info2() {
+        String testo = "";
+        if (isValido2() == true) {
+            if (giorno >= 1 && giorno <= 9) {
+                testo = testo = "0" + giorno + "/";
+            } else {
+                testo = giorno + "/" + mese + "/" + anno;
+            }
+            if (mese >= 1 && mese <= 9) {
+                testo += "0" + mese + "/" + anno;
+            } else {
+                testo = giorno + "/" + mese + "/" + anno;
+            }
+        } else {
+            testo = "data non valida";
+        }
+        return testo;
+    }
+
+    public int distanzaGiorni() {
+        int gDiff = 0;
+        int mesi[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int mesi2[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int nGData1 = 0;
+        int nGData2 = 0;
+        if (isBisestile() == true) {
+            mesi[1] = 29;
+        }
+        if (isBisestile2() == true) {
+            mesi[1] = 29;
+        }
+        nGData1 = g + a * 365 + (mesi[m + 1] * m);
+        nGData2 = giorno + anno * 365 + (mesi[mese + 1] * mese);
+        if (nGData1 > nGData2) {
+            gDiff = nGData1 - nGData2;
+        } else {
+            gDiff = nGData2 - nGData1;
+           
+        }
+        return gDiff;
+    }
 }
+/*
+   
+    public int distanzaGiorni(int g , int m , int a) {
+        int gDiff = 0;
+        int mesi[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int mesi2[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (isBisestile() == true) {
+            mesi[1] = 29;
+        }
+        if (isBisestile2() == true) {
+            mesi[1] = 29;
+        }
+        giorno = anno*365;
+        giorno += g;
+       
+        return gDiff;
+    }
+}
+*/
--- src/cicli/DataTest2.java
+++ src/cicli/DataTest2.java
@@ -11,17 +11,27 @@ public class DataTest2 {
         int a;
         int m;
         int g;
+        int giorno;
+        int mese;
+        int anno;
         
         System.out.println("Inserire il giorno, il mese e l'anno: ");
         a = in.nextInt();
         m = in.nextInt();
         g = in.nextInt();
         
-        Data d = new Data(g,m,a);
+        System.out.println("Inserire il giorno, il mese e l'anno della seconda data: ");
+        anno = in.nextInt();
+        mese = in.nextInt();
+        giorno = in.nextInt();
+        
+        Data2 d = new Data2(g,m,a,giorno,mese,anno);
                 
-        System.out.println("La data è: ");
+        System.out.println("La prima data è: ");
         System.out.println(d.info());
-        System.out.println("L'anno è bisestile: " + d.isBisestile());
+        System.out.println("La seconda data è: ");
+        System.out.println(d.info2());
+        System.out.println("La differenza è: " + d.distanzaGiorni());
         
     }
     
